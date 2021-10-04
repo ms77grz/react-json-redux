b67b36f595d4825d4a429ba49375088fa0eb096e
Magomed Shamaev <ms77grz@gmail.com>
2021-10-05 00:28:02 +0300

    Added CustomizedTables

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import CustomizedTables from '../components/CustomizedTables';
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <CustomizedTables />
+    </div>
+  );
+};
